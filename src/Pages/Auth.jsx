340abf8bfa520c3441c728add9c3f3c624fedc97
22-jobu <kamaujob2@gmail.com>
2024-11-06 10:47:54 +0300

    login

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -5,6 +5,7 @@ function Auth() {
       <div className='content-center p-10 bg-gray-200 m-10 rounded-lg w-[500px]'>
         <form action="">
             <h1 className='align-center font-bold'>Registration form</h1>
+            <h4>Login</h4>
             <input type="text" placeholder='Full Name' className='w-full my-5 p-1 text-center rounded-xl'/>
             <br />
             <input type="email" placeholder='email' className='w-full my-5 p-1 text-center rounded-xl' />
