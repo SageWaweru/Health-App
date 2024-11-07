02d4559592f3adee0908f95cf0299195e15c8c6c
Joy Ngugi <ngugijoy51@gmail.com>
2024-11-07 09:55:51 +0300

    join

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,18 +1,18 @@
 function Auth() {
   return (
     <div>
-      Auth
+      
       <div className='content-center p-10 bg-gray-200 m-10 rounded-lg w-[500px]'>
         <form action="">
-            <h1 className='align-center font-bold'>Registration form</h1>
-            <h4>Login</h4>
+            <h1 className='text-2xl align-center font-bold'>Enter Your Details</h1>
+            <h4 className="text-sm font-bold">LOGIN</h4>
             <input type="text" placeholder='Full Name' className='w-full my-5 p-1 text-center rounded-xl'/>
             <br />
-            <input type="email" placeholder='email' className='w-full my-5 p-1 text-center rounded-xl' />
+            <input type="email" placeholder='Email' className='w-full my-5 p-1 text-center rounded-xl' />
             <br />
-            <input type="password" placeholder='password' className='w-full my-5 p-1 text-center rounded-xl' />
+            <input type="password" placeholder='Password' className='w-full my-5 p-1 text-center rounded-xl' />
             <br />
-            <button className='bg-green-300 p-1 px-5 w-full my-5'>submit</button>
+            <button className='bg-cyan-800 text-white font-semibold hover:bg-cyan-600 p-1 px-5 w-full my-5'>submit</button>
 
             
         </form>
