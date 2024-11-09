d3f9c1e3d2795411e66257871e099067dc61ba43
Joy Ngugi <ngugijoy51@gmail.com>
2024-11-09 13:28:00 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import MentalHealth from './Pages/MentalHealth';
 import Dashboard from './Pages/Dashboard';
-import Goals from './Pages/Goals';
+// import Goals from './Pages/Goals';
 import { GoalsProvider } from './Context/GoalsProvider';
 import Reminder from './Components/Reminder';
 import SupportCommunity from './Pages/SupportCommunity';
